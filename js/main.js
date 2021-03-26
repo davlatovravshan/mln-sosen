e018ecbed15e45101a4774d2520d124ac92e93ba
Ravshan Davlatov <ravshandavlatov014@gmail.com>
2021-03-26 11:49:10 +0500

    Validation to form done

--- js/main.js
+++ js/main.js
@@ -45,11 +45,109 @@ $(window).on('scroll load resize', function() {
 
 // Валидация Формы
 
+let requiredinputs = [
+	'name',
+	'email',
+	'theme',
+];
+
 let form = $('.contact-form');
 
 form.on('submit', function(event) {
 
-	console.log(form.elements)
+	event.preventDefault();
 
-	event.preventDefault();	
-})
+	var myForm = $(this);
+
+	var thisForm = myForm.get(0);
+	var formElements = thisForm.elements;
+
+	var formErrors = [];
+	for (var i = 0; i < formElements.length - 1; i++) {
+
+		var element = formElements[i];
+
+		if (!requiredinputs.includes(element.name)) {
+			continue;
+		}
+
+		if (element.value == '') {
+			formErrors.push(element.name)
+		}
+
+	}
+
+	var emailInput = formElements['email'];
+	if (!validateEmail(emailInput.value) && !formErrors.includes('email')) {
+		formErrors.push('email');
+	}
+
+	var enableForm = showErrorOnSubmit(formErrors, myForm);
+	console.log(enableForm);
+});
+
+
+let formInputs = $('.form-input');
+
+formInputs.on('change keyup focusout', function() {
+
+	if (!requiredinputs.includes(this.name)) {
+		return false;
+	}
+
+	if (this.value == '') {
+		addError(this);
+	} else {
+		noError(this);
+	}
+
+});
+
+
+let emailInput = $('.form-input[type=email]');
+
+emailInput.on('change keyup focusout', function() {
+
+	var value = this.value;
+
+	if (!validateEmail(value)) {
+		addError(this);
+	} else {
+		noError(this);
+	}
+
+});
+
+
+function showErrorOnSubmit(formErrors, myForm) {
+
+	var enableForm = true;
+	formErrors.forEach(function(name) {
+		
+		var element = myForm.find('[name=' + name + ']');
+
+		if (element.val() == '') {
+			addError(element.get(0));
+			enableForm = false;
+		}
+
+	});
+
+	return enableForm;
+
+}
+
+function addError(element) {
+	$(element).addClass('form-input-error');
+	$(element).siblings('.form-error').show();	
+}
+
+function noError(element) {
+	$(element).removeClass('form-input-error');
+	$(element).siblings('.form-error').hide();
+}
+
+function validateEmail(email) {
+	const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(email);
+}
